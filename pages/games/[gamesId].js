af8a59fc4827437e639bc03f7089297c31b63c36
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-31 16:28:45 +0530

    Linking in Next

--- pages/games/[gamesId].js
+++ pages/games/[gamesId].js
@@ -1,12 +1,15 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 
 // Dynamic Route with []
 // Will load for anything after 3000/games/details
-
+    
 function DetailPage() {
-    const router = useRouter();
-    console.log(router.query.gamesId)
-    return <h1>My Game's Detail</h1>
-  }
-  
-export default DetailPage
+  const router = useRouter();
+  const gamesId = router.query.gamesId;
+
+  // we can use this id and send it to the backend server
+  // to fetch the news item with gamesId
+  return <h1>My Game's Detail</h1>;
+}
+
+export default DetailPage;
--- pages/games/index.js
+++ pages/games/index.js
@@ -1,5 +1,22 @@
-function games() {
-    return <h1>My Games Page</h1>
-  }
-  
-  export default games
+import Link from 'next/link';
+import { Fragment } from "react";
+
+function GamesPage() {
+  return (
+    <Fragment>
+      <h1> The Games Page</h1>
+      <ul>
+        <li>
+          <a href="/games/js-games">Games build with JS</a>
+          {/* Here we get the page but it is not a how react single page works we load at fetching */}
+        </li>
+        <li>
+          <Link href="/games/regular-games"> Games built without JS</Link></li>
+          {/* Notice we do not fetch anything when we use the Link tag and provides single page application functionality
+          and therfore it is the preferred manner of using links in the next application */}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default GamesPage;
