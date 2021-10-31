20c67f35374f7eb5c7c188de060fb324b1391227
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-31 15:35:28 +0530

    Making use of dynamic url

--- pages/games/[gamesId].js
+++ pages/games/[gamesId].js
@@ -1,7 +1,11 @@
+import {useRouter} from 'next/router';
+
 // Dynamic Route with []
 // Will load for anything after 3000/games/details
 
 function DetailPage() {
+    const router = useRouter();
+    console.log(router.query.gamesId)
     return <h1>My Game's Detail</h1>
   }
   
